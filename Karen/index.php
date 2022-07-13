de0eaf55a3b6033e70466f30c31780e20ee23915
Enrique Yair Ramos Resendiz <enrique_rr@tesch.edu.mx>
2022-07-12 19:30:26 -0500

    Commit inicial.

---
+++ Karen/index.php
@@ -0,0 +1,27 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+  <link rel="stylesheet" href="https://unpkg.com/jcrop/dist/jcrop.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/jcrop"></script>
+</head>
+<body>
+<input type="file" id="imagen" name="imagen" accept="image/*">
+    <br><br>
+    <!-- La imagen que vamos a usar para previsualizar lo que el usuario selecciona -->
+    <img id="imagenPrevisualizacion">
+  <script type="text/javascript">
+    $('#imagen').change(function() {
+    const imagen = $('#imagen').val();
+    console.log (imagen);
+    $('#imagenPrevisualizacion').prop('src', imagen);
+    });
+  </script>
+</body>
+</html>
